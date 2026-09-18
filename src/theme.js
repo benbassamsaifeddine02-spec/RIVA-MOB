d14850f2da924e71e6847250feed2d77d00bd736
benbassamsaifeddine02-spec <benbassam.saifeddine02@gmail.com>
2026-09-18 15:55:26 +0100

    Add RIVA design theme

---
+++ src/theme.js
@@ -0,0 +1,12 @@
+export const C = {
+  red: '#EF2B20',
+  redDark: '#C92018',
+  orange: '#F2A51B',
+  ink: '#202428',
+  gray: '#6F767C',
+  line: '#D9DDE0',
+  soft: '#F5F6F7',
+  white: '#FFFFFF',
+  green: '#198754'
+};
+export const R = { sm:8, md:12, lg:16 };
